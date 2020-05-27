fa6093f4921eb5311dc360eea62c3d246f22b8db
Rahul Gajawada <rahulgajawada@Rahuls-MacBook-Pro.local>
2020-05-27 12:40:14 -0500

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=5486d3e4f1fb39c6be05e6ba589f5160&query='+latitude+','+longitude+'&units=f'
+    request({url, json: true}, (error, {body}) => {
+        if(error)
+            callback('Unable to connect to the internet', undefined)
+        else if(body.error)
+            callback('Invalid coordinates have been provided', undefined)
+        else{
+            const data = body.current.weather_descriptions[0] + " :It is currently " + body.current.temperature + " degrees out. There is a " + body.current.precip +"% chance of rain"
+            callback(undefined,data)
+        }
+    })
+}
+
+module.exports = forecast
